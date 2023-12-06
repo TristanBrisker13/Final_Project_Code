04a302df8c8b5186279e0b1a16239c36fc867142
TristanBrisker13 <148290322+TristanBrisker13@users.noreply.github.com>
2023-12-05 22:46:27 -0500

    Add files via upload

---
+++ system.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "system.h"
+using namespace std;
+
+int main()
+{
+    cout << "\n";
+    cout << "WELCOME----TO----THE----LOGIN----AND----REGISTRATION----SYSTEM" << endl;
+    cout << "\n";
+    class system c1;
+    c1.main_menu();
+}
