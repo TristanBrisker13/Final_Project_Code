ea09e40defb8c157a37572eb12d38c03142b133a
TristanBrisker13 <148290322+TristanBrisker13@users.noreply.github.com>
2025-03-25 22:34:17 -0400

    Update system.cpp

--- system.cpp
+++ system.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     cout << "\n";
-    cout << "WELCOME----TO----THE----LOGIN----AND----REGISTRATION----SYSTEM" << endl;
+    cout << "WELCOME----TO----THE----LOGIN----AND----REGISTRATION----SYSTEM-----MADE------BY-----TRISTAN" << endl;
     cout << "\n";
     class system c1;
     c1.main_menu();
